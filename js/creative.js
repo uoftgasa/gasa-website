a8ca35c9857bd35713f2d28b458b1e3ed735dedb
Henry Leung <henryskyleung@gmail.com>
2019-09-27 22:59:47 -0400

    switch to bootstrap 4, fontawesome 5, from LESS to SCSS

--- js/creative.js
+++ js/creative.js
@@ -1,52 +1,46 @@
-(function ($) {
+(function($) {
   "use strict"; // Start of use strict
 
-  // jQuery for page scrolling feature - requires jQuery Easing plugin
-  $('a.page-scroll').bind('click', function (event) {
-    var $anchor = $(this);
-    $('html, body').stop().animate({
-      scrollTop: ($($anchor.attr('href')).offset().top - 50)
-    }, 500, 'easeInOutExpo');
-    event.preventDefault();
-  });
-
-  // Highlight the top nav as scrolling occurs
-  $('body').scrollspy({
-    target: '.navbar-fixed-top',
-    offset: 51
-  });
-
-  // Closes the Responsive Menu on Menu Item Click
-  $('.navbar-collapse ul li a').click(function () {
-    $('.navbar-toggle:visible').click();
-  });
-
-  // Offset for Main Navigation
-  $('#mainNav').affix({
-    offset: {
-      top: 100
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 72)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
     }
-  })
-
-  // Initialize and Configure Scroll Reveal Animation
-  window.sr = ScrollReveal();
-  sr.reveal('.sr-icons', {
-    duration: 600,
-    scale: 0.3,
-    distance: '0px'
-  }, 200);
-  sr.reveal('.sr-button', {
-    duration: 1000,
-    delay: 200
   });
-  sr.reveal('.sr-contact', {
-    duration: 600,
-    scale: 0.3,
-    distance: '0px'
-  }, 300);
 
-  // Initialize and Configure Magnific Popup Lightbox Plugin
-  $('.popup-gallery').magnificPopup({
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 75
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-scrolled");
+    } else {
+      $("#mainNav").removeClass("navbar-scrolled");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Magnific popup calls
+  $('#portfolio').magnificPopup({
     delegate: 'a',
     type: 'image',
     tLoading: 'Loading image #%curr%...',
@@ -54,13 +48,12 @@
     gallery: {
       enabled: true,
       navigateByImgClick: true,
-      preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+      preload: [0, 1]
     },
     image: {
       tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
     }
   });
-
   // Load GASA members list
   $(function () {
     $('#members-list').load("members19_20.html");
@@ -74,5 +67,4 @@
       scrollTop: panel.offset().top
     }, 500, 'easeInOutExpo');
   });
-
 })(jQuery); // End of use strict
